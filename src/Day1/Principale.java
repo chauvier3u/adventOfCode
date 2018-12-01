2252899d2edbc4e2fa7ac748350ca1ac28255601
chauvier3u <benoitchauviere@hotmail.fr>
2018-12-01 11:50:21 +0100

    réussi Day1 challenge 2

--- src/Day1/Principale.java
+++ src/Day1/Principale.java
@@ -2,11 +2,18 @@ package Day1;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Principale
 {
 	public static void main(String[] args) throws FileNotFoundException
+	{
+		partOne();
+		partTwo();
+	}
+
+	public static void partOne() throws FileNotFoundException
 	{
 		File fichier = new File("./src/Day1/data.txt");
 		Scanner sc = new Scanner(fichier);
@@ -24,5 +31,46 @@ public class Principale
 		sc.close();
 
 		System.out.println(frequence);
+	}
+
+	public static void partTwo() throws FileNotFoundException
+	{
+		File fichier = new File("./src/Day1/data.txt");
+		Scanner sc = new Scanner(fichier);
+		String ligne = "";
+
+		int frequence = 0;
+		int tmpFrequence = 0;
+		int nbBoucle = 0;
+		ArrayList<Integer> frequenceUnique = new ArrayList<Integer>();
+		frequenceUnique.add(0);
+
+		boolean continuer = true;
+
+		while (continuer)
+		{
+			if (!sc.hasNextLine())
+			{
+				sc.close();
+				sc = new Scanner(fichier);
+				nbBoucle++;
+			}
+			ligne = sc.nextLine();
+			tmpFrequence = Integer.parseInt(ligne);
+			frequence += tmpFrequence;
+			frequenceUnique.add(frequence);
+			for (int i = 0; i < frequenceUnique.size() - 1; i++)
+			{
+				if (frequence == frequenceUnique.get(i))
+				{
+					continuer = false;
+				}
+			}
+
+		}
+		sc.close();
+
+		System.out.println(frequence);
+		System.out.println("avec " + nbBoucle + " boucles");
 	}
 }
