28b28589ed34873deaeb87126e90513e8c78755c
chauvier3u <benoitchauviere@hotmail.fr>
2018-12-01 11:24:31 +0100

    reussi Day1 challenge 1

--- src/Day1/Principale.java
+++ src/Day1/Principale.java
@@ -1,10 +1,28 @@
 package Day1;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Principale
 {
-	public static void main(String[] args)
+	public static void main(String[] args) throws FileNotFoundException
 	{
-		// TODO Auto-generated method stub
+		File fichier = new File("./src/Day1/data.txt");
+		Scanner sc = new Scanner(fichier);
+		String ligne = "";
 
+		int frequence = 0;
+		int tmpFrequence = 0;
+
+		while (sc.hasNextLine())
+		{
+			ligne = sc.nextLine();
+			tmpFrequence = Integer.parseInt(ligne);
+			frequence += tmpFrequence;
+		}
+		sc.close();
+
+		System.out.println(frequence);
 	}
 }
